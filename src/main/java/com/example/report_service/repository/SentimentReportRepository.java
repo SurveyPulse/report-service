3bdd3d51f419e294cc8a5387a39b00ad4441c5b4
201912025 <michi2012@naver.com>
2025-04-03 14:57:43 +0900

    feat: OverallSentimentReport 레포지토리 구현 및 SentimentReportRepository 수정

--- src/main/java/com/example/report_service/repository/SentimentReportRepository.java
+++ src/main/java/com/example/report_service/repository/SentimentReportRepository.java
@@ -5,6 +5,11 @@ import com.example.report_service.entity.SentimentReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SentimentReportRepository extends JpaRepository<SentimentReport, Long> {
+
+    List<SentimentReport> findBySurveyId(Long surveyId);
+
 }
