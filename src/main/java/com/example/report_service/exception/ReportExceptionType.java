554b1c4087417111d6448dc184000df2d505080f
201912025 <michi2012@naver.com>
2025-04-03 12:51:30 +0900

    feat: 예외타입 구현

--- src/main/java/com/example/report_service/exception/ReportExceptionType.java
+++ src/main/java/com/example/report_service/exception/ReportExceptionType.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ReportExceptionType implements ExceptionType {
+public enum ReportExceptionType implements ExceptionType {
+    REPORT_NOT_FOUND(6201, "해당 분석 데이터를 찾을 수 없습니다."),
+    REPORT_SAVE_ERROR(6202, "분석 데이터 저장 중 오류가 발생했습니다."),
+    INVALID_REPORT_DATA(6203, "분석 데이터가 올바르지 않습니다.");
 
     private final int statusCode;
     private final String message;
